3c3ab72221837198e4708d0ee3129e95a62bfa4a
Nada Amin <nada.amin@epfl.ch>
2012-07-13 16:49:27 +0200

    update to M5

--- build.sbt
+++ build.sbt
@@ -4,7 +4,7 @@ organization := "EPFL"
 
 version := "0.1"
 
-scalaVersion := "2.10.0-M4"
+scalaVersion := "2.10.0-M5"
 
 resolvers += ScalaToolsSnapshots
 
--- src/main/scala/scala/reflect/Defaults.scala
+++ src/main/scala/scala/reflect/Defaults.scala
@@ -5,9 +5,8 @@ import definitions._
 
 object Defaults {
   def apply(x: Type): AnyRef = {
-    // workaround SI-5959
-    if (x.typeSymbol == UnitTpe.typeSymbol) (().asInstanceOf[AnyRef])
-    else if (x.typeSymbol == IntTpe.typeSymbol)  (0: java.lang.Integer)
+    if (x =:= UnitTpe) (().asInstanceOf[AnyRef])
+    else if (x =:= IntTpe)  (0: java.lang.Integer)
     else ???
   }
 }
